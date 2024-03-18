0f1b704b2fbe3cf01e394c756c8138b881b894c7
InNoobWeTrust <vu.truong.16193@gmail.com>
2024-03-18 21:28:48 +0700

    feat: POC

---
+++ src/app.rs
@@ -0,0 +1,75 @@
+use leptos::leptos_dom::ev::SubmitEvent;
+use leptos::logging::log;
+use leptos::*;
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "window", "appWindow"])]
+    async fn hide() -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    let (name, set_name) = create_signal(String::new());
+    let (greet_msg, set_greet_msg) = create_signal(String::new());
+
+    let update_name = move |ev| {
+        let v = event_target_value(&ev);
+        set_name.set(v);
+    };
+
+    let greet = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        spawn_local(async move {
+            let name = name.get_untracked();
+            if name.is_empty() {
+                return;
+            }
+
+            let args = to_value(&GreetArgs { name: &name }).unwrap();
+            // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+            let new_msg = invoke("greet", args).await.as_string().unwrap();
+            set_greet_msg.set(new_msg);
+        });
+    };
+    let esc_handle = window_event_listener(ev::keypress, |ev| {
+        // ev is typed as KeyboardEvent automatically,
+        // so .code() can be called
+        let code = ev.code();
+        log!("code = {code:?}");
+        if code == "Escape" {
+            spawn_local(async move {
+                hide().await;
+            });
+        }
+    });
+    on_cleanup(move || {
+        esc_handle.remove();
+    });
+
+    view! {
+        <main class="container">
+            <form class="row" on:submit=greet>
+                <input
+                    autoFocus
+                    id="greet-input"
+                    placeholder="Enter a name..."
+                    on:input=update_name
+                />
+                <button type="submit">"Greet"</button>
+            </form>
+
+            <p><b>{ move || greet_msg.get() }</b></p>
+        </main>
+    }
+}
