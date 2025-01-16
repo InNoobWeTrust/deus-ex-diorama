bef014b51266e5d9dcd20f0855bcea8e761ec33b
InNoobWeTrust <vu.truong.16193@gmail.com>
2025-01-17 05:33:03 +0700

    refactor: use native web components for UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,34 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::GlobalShortcutManager;
-use tauri::Manager;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            let main_window = app.get_window("main").unwrap();
-            main_window.set_decorations(false)?;
-            app.global_shortcut_manager()
-                .register("CmdOrControl+Alt+Shift+A", move || {
-                    if let Ok(visible) = main_window.is_visible() {
-                        if visible {
-                            main_window.hide().unwrap();
-                        } else {
-                            main_window.show().unwrap();
-                            main_window.set_focus().unwrap();
-                        }
-                    }
-                })?;
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  app_lib::run();
 }
